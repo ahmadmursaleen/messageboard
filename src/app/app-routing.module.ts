3f4d7f4fa4a65f82d60230b8c82cf447ce1c7832
Ahmad Mursaleen <ahmadmursaleen@gmail.com>
2019-09-18 15:24:04 +0200

    routing to message component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MessagesComponent } from "./messages/messages.component";
+import { FullscreenOverlayContainer } from "@angular/cdk/overlay";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "messages"
+  },
+  {
+    path: "messages",
+    component: MessagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
